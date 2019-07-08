a05ce03879e311e91d601208fa43230985184e9b
kangkimsim <kangkimsim@gmail.com>
2019-07-08 11:24:57 +0800

    update

--- src/components/multiSelectedValue.js
+++ src/components/multiSelectedValue.js
@@ -1,6 +1,6 @@
 const multiSelectedProfile = ["techFlow.SupportedOperationMode"]    //Profile with multi-selected value
 
-const multiSelectedPairedProfile = [] //Paired Profile (profile with .IfParentNotFound) with multi-selected value
+const multiSelectedPairedProfile = [""] //Paired Profile (profile with .IfParentNotFound) with multi-selected value
 
 export  {multiSelectedProfile,
         multiSelectedPairedProfile}
