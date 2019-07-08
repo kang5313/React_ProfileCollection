bcfec91c53abf0cf281373676f38fb75575bd92c
kangkimsim <kangkimsim@gmail.com>
2019-07-08 11:44:54 +0800

    Update

--- src/components/multiSelectedValue.js
+++ src/components/multiSelectedValue.js
@@ -1,8 +1,6 @@
-const multiSelectedProfile = ["techFlow.SupportedOperationMode",
-"techFlow.ShowTargetPlatformDropdown",
-"techFlow.ShowTargetPlatformDropdown.IfParentNotFound"]    //Profile with multi-selected value
+const multiSelectedProfile = ["techFlow.SupportedOperationMode"]    //Profile with multi-selected value
 
-const multiSelectedPairedProfile = ["techFlow.ShowTargetPlatformDropdown"] //Paired Profile (profile with .IfParentNotFound) with multi-selected value
+const multiSelectedPairedProfile = [] //Paired Profile (profile with .IfParentNotFound) with multi-selected value
 
 export  {multiSelectedProfile,
         multiSelectedPairedProfile}
