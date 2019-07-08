f578006053467f405387e82399609c5296cac6f7
kangkimsim <kangkimsim@gmail.com>
2019-07-08 11:13:36 +0800

    8/7 bugs fix

--- src/components/multiSelectedValue.js
+++ src/components/multiSelectedValue.js
@@ -1,8 +1,6 @@
-const multiSelectedProfile = ["techFlow.SupportedOperationMode",
-                              "techFlow.SupportedTargetPlatform",
-                              "techFlow.SupportedTargetPlatform.IfParentNotFound"]    //Profile with multi-selected value
+const multiSelectedProfile = ["techFlow.SupportedOperationMode"]    //Profile with multi-selected value
 
-const multiSelectedPairedProfile = ["techFlow.SupportedTargetPlatform"] //Paired Profile (profile with .IfParentNotFound) with multi-selected value
+const multiSelectedPairedProfile = [] //Paired Profile (profile with .IfParentNotFound) with multi-selected value
 
 export  {multiSelectedProfile,
         multiSelectedPairedProfile}
